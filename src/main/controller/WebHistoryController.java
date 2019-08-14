4a3d16d1a705fc3ed0c82f6adf2b12c0b4835b5e
Ante Skoric <ante.skoric@haw-hamburg.de>
2019-08-14 14:33:15 +0200

    Added some comments, changed the initialize method

--- src/main/controller/WebHistoryController.java
+++ src/main/controller/WebHistoryController.java
@@ -107,4 +107,5 @@ public class WebHistoryController implements Initializable {
 
         elementsView.setItems(FXCollections.observableArrayList(HistoryDataExtractor.selectSearchHistory()));
     }
+
 }
