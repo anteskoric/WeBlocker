df8835142f9e73ec6c5ff2dd5cb2cad93eafe885
Ante Skoric <ante.skoric@haw-hamburg.de>
2019-08-14 13:52:31 +0200

    Added setters and comments

--- src/main/database/classes/Cookie.java
+++ src/main/database/classes/Cookie.java
@@ -31,14 +31,38 @@ import java.util.Objects;
  */
 public class Cookie {
 
+    /**
+     * Name of the cookie
+     */
     private String name;
+
+    /**
+     * Host key of the cookie
+     */
     private String hostKey;
-    private boolean hasExpired;
+
+    /**
+     * Is the cookie expired
+     * 0 if it is expired else 1
+     */
+    private int hasExpired;
+
+    /**
+     * The time of creation
+     */
     private LocalDateTime creationTime;
+
+    /**
+     * The time and date when the cookie expires
+     */
     private LocalDateTime expiresTime;
+
+    /**
+     * Last access of the cookie
+     */
     private LocalDateTime lastAccessTime;
 
-    public Cookie(String name, String hostKey, boolean hasExpired, LocalDateTime creationTime, LocalDateTime expiresTime, LocalDateTime lastAccessTime){
+    public Cookie(String name, String hostKey, int hasExpired, LocalDateTime creationTime, LocalDateTime expiresTime, LocalDateTime lastAccessTime){
         this.name = name;
         this.hostKey = hostKey;
         this.hasExpired = hasExpired;
@@ -47,6 +71,9 @@ public class Cookie {
         this.lastAccessTime = lastAccessTime;
     }
 
+    /**
+     * Field getters
+     */
     public String getName() {
         return this.name;
     }
@@ -55,7 +82,7 @@ public class Cookie {
         return this.hostKey;
     }
 
-    public boolean getHasExpired() {
+    public int getHasExpired() {
         return this.hasExpired;
     }
 
@@ -71,6 +98,38 @@ public class Cookie {
         return this.lastAccessTime;
     }
 
+    /**
+     * Field setters
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHostKey(String hostKey) {
+        this.hostKey = hostKey;
+    }
+
+    public void setHasExpired(int hasExpired) {
+        this.hasExpired = hasExpired;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public void setExpiresTime(LocalDateTime expiresTime) {
+        this.expiresTime = expiresTime;
+    }
+
+    public void setLastAccessTime(LocalDateTime lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    /**
+     *
+     * @param o Object to be verified
+     * @return boolea true if the objects are equal else false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -84,11 +143,18 @@ public class Cookie {
                 Objects.equals(lastAccessTime, cookie.lastAccessTime);
     }
 
+    /**
+     * Generates hash code of the object
+     * @return hash code of the object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, hostKey, hasExpired, creationTime, expiresTime, lastAccessTime);
     }
 
+    /**
+     * @return object in the form of string
+     */
     @Override
     public String toString() {
         return "Cookie{" +
