7b2eed1aec51dcd8c6ee655be3d5eb69f6c4b5e8
Ante Skoric <ante.skoric@haw-hamburg.de>
2019-08-15 16:00:44 +0200

    Added onActionTableView method and some comments

--- src/main/controller/CookiesController.java
+++ src/main/controller/CookiesController.java
@@ -23,6 +23,7 @@ package controller;
 // THE SOFTWARE.
 
 import database.classes.Cookie;
+import exceptions.SelectedColumnIsEmptyException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -33,7 +34,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import logic.CookiesDataExtractor;
 
 import java.net.URL;
-import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 /**
@@ -71,19 +71,19 @@ public class CookiesController implements Initializable {
      * creationDateColumn is TableColumn of the TableView cookiesTable it displays the creationTime of the object
      */
     @FXML
-    private TableColumn<Cookie, LocalDateTime> creationDateColumn;
+    private TableColumn<Cookie, String> creationDateColumn;
 
     /**
      * expiresDateColumn is TableColumn of the TableView cookiesTable it displays the expiresTime of the object
      */
     @FXML
-    private TableColumn<Cookie,LocalDateTime> expiresDateColumn;
+    private TableColumn<Cookie, String> expiresDateColumn;
 
     /**
      * lastAccessColumn is TableColumn of the TableView cookiesTable it displays the lastAccessTime of the object
      */
     @FXML
-    private TableColumn<Cookie, LocalDateTime> lastAccessColumn;
+    private TableColumn<Cookie, String> lastAccessColumn;
 
     /**
      * Called to initialize a controller after its root element has been
@@ -97,12 +97,39 @@ public class CookiesController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         hostKeyColumn.setCellValueFactory(new PropertyValueFactory<>("hostKey"));
-        hasExpiredColumn.setCellValueFactory(new PropertyValueFactory<>("hasExpiredColumn"));
+        hasExpiredColumn.setCellValueFactory(new PropertyValueFactory<>("hasExpired"));
         creationDateColumn.setCellValueFactory(new PropertyValueFactory<>("creationTime"));
         expiresDateColumn.setCellValueFactory(new PropertyValueFactory<>("expiresTime"));
         lastAccessColumn.setCellValueFactory(new PropertyValueFactory<>("lastAccessTime"));
 
+        addElementsIntoTableView();
+    }
+
+    /**
+     * Adds Elements into TableView
+     */
+    private void addElementsIntoTableView() {
         ObservableList<Cookie> cookiesList = FXCollections.observableArrayList(CookiesDataExtractor.selectCookies());
         cookiesTable.setItems(cookiesList);
+    }
+
+    /**
+     * Deletes cookies from the db and updates the TableView
+     * @throws SelectedColumnIsEmptyException if selected row is empty throws the exception
+     */
+    @FXML
+    public void onActionTableView(){
+        isColumnNull();
+        CookiesDataExtractor.deleteCookie(cookiesTable.getSelectionModel().getSelectedItem().getCreationUtc(),cookiesTable.getSelectionModel().getSelectedItem().getName(),cookiesTable.getSelectionModel().getSelectedItem().getHostKey());
+        addElementsIntoTableView();
+    }
+
+    /**
+     * Check if the selected row is empty
+     * @throws SelectedColumnIsEmptyException if empty
+     */
+    private void isColumnNull() {
+        if(cookiesTable.getSelectionModel().isEmpty())
+            throw new SelectedColumnIsEmptyException("The selected row is empty");
     }
 }
