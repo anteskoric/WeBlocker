a8d7405144d3403e5e10daf679335d598f956b78
Ante Skoric <ante.skoric@haw-hamburg.de>
2019-08-27 20:08:25 +0200

    Added the class WebsiteBlocker that tracks the website usage and it deserializes the json objects and compares the average website usage with the allowed website usage

---
+++ src/main/logic/WebsiteBlocker.java
@@ -0,0 +1,78 @@
+package logic;
+
+// The MIT License
+//
+//Copyright (c) 2010-2019 Google, Inc. http://angularjs.org
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import interfaces.DataBaseConnector;
+import json.classes.WebsiteVisitTracer;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+/**
+ * The class WebsiteBlocker is a utility class for checking if the json web url objects should be blocked or not.
+ * If the object from the VisitTracer.json is older then one week it will be read
+ * and it will be checked if the average usage is grater then the users input, if the usage is greater the website
+ * will be blocked
+ * The class is called from MainMenuController and it reads objects from VisitTracer.json
+ *
+ * @author Ante Skoric
+ */
+public final class WebsiteBlocker implements DataBaseConnector {
+
+    private WebsiteBlocker(){}
+
+    /**
+     * The method checkWebsiteBlockage gets all objects that are older than one week
+     * from VisitTracer.json and calls checkWebsiteUsage
+     */
+    public static void checkWebsiteBlockage(){
+        LocalDateTime currentTime = LocalDateTime.now(ZoneId.of("Europe/Berlin"));
+        long currentTimeInSeconds = DateManager.getSecondsFromUTF(currentTime);
+        long sevenDaysInSeconds = 604800;
+        List<WebsiteVisitTracer> tracers = IOJson.getJsonObjects();
+        for (WebsiteVisitTracer tracer : tracers) {
+            if(tracer.getEntryCreation() <= (currentTimeInSeconds - sevenDaysInSeconds)){
+                checkWebsiteUsage(tracer,tracers);
+            }
+        }
+    }
+
+    /**
+     * The method checkWebsiteUsage checks if the average daily usage of the website is
+     *  greater then allowed daily usage (Users input), if it is greater the website will be blocked
+     * @param tracer The WebsiteVisitTracer object that should be checked
+     * @param tracers The List of all WebsiteVisitTracer objects that are read from the json file
+     */
+    private static void checkWebsiteUsage(WebsiteVisitTracer tracer, List<WebsiteVisitTracer> tracers) {
+        long allowedDailyUsage = tracer.getHours();
+        //TODO sql script
+        long averageDailyUsage = 0;
+
+
+        if(averageDailyUsage > allowedDailyUsage)
+            IOHosts.writeIntoHost(IOHosts.getHostName(tracer.getUrl()));
+        tracers.remove(tracer);
+        IOJson.overwriteJson(tracers);
+    }
+}
