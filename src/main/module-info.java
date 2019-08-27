91a158fc66a7b13df558d16b839b6b17a9b1508a
Ante Skoric <ante.skoric@haw-hamburg.de>
2019-08-27 12:20:06 +0200

    Added gson, json.simple and jackson librarys

--- src/main/module-info.java
+++ src/main/module-info.java
@@ -4,6 +4,9 @@ module JavaFxApplication {
     requires javafx.graphics;
     requires java.base;
     requires java.sql;
+    requires json.simple;
+    requires gson;
+    requires com.fasterxml.jackson.core;
 
     opens controller;
     opens logic;
